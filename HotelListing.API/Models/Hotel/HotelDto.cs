3dfb1186ab529b8991a305346bc83057c0425dfb
jebarcha <jebarcha@hotmail.com>
2022-04-22 16:14:55 -0500

    Added Hotels Controller and Hotels Repository

--- HotelListing.API/Models/Hotel/HotelDto.cs
+++ HotelListing.API/Models/Hotel/HotelDto.cs
@@ -1,11 +1,12 @@
 ﻿namespace HotelListing.API.Models
 {
-    public class HotelDto
+    public class HotelDto : BaseHotelDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public double Rating { get; set; }
-        public int CountryId { get; set; }
     }
+    public class CreateHotelDto : BaseHotelDto
+    {
+       
+    }
+
 }
